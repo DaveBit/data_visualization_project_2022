a511c9caf904491c2bd4e5a45c8ef66fa9625bf2
DaveBit <dewidhoct@gmail.com>
2022-12-30 00:30:11 +0100

    Chapter 15

--- chapter15/rw_visual.py
+++ chapter15/rw_visual.py
@@ -6,7 +6,18 @@ while True:
     rw = RandomWalk()
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=10)
+    point_numbers = list(range(rw.num_points))  # Refresher, range is a function that returns a sequence of numbers.
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolor='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
+
+    # Remove the axes
+    plt.gca().get_xaxis().set_visible(False)
+    plt.gca().get_yaxis().set_visible(False)
+
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
