5912fe6bd9080d4a3e23f1653381f93a8bb0ede3
DaveBit <dewidhoct@gmail.com>
2022-12-31 12:23:40 +0100

    Chapter 15

--- chapter15/rw_visual.py
+++ chapter15/rw_visual.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Make a random walk, and plot the points.
 while True:
-    rw = RandomWalk()
+
+    # Make a random walk, and plot the points.
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # Set the size of the plotting window.
+    plt.figure(figsize=(12, 6))
+
     point_numbers = list(range(rw.num_points))  # Refresher, range is a function that returns a sequence of numbers.
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=10)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolor='none', s=100)
@@ -17,9 +21,9 @@ while True:
     plt.gca().get_xaxis().set_visible(False)
     plt.gca().get_yaxis().set_visible(False)
 
-
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
         break
+
