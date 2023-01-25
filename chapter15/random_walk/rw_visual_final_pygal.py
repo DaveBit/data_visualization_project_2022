abbc9244e091a0aacc3382d19b5f61ee0411a531
DaveBit <dewidhoct@gmail.com>
2023-01-25 23:01:07 +0100

    Chapter 15

--- chapter15/random_walk/rw_visual_final_pygal.py
+++ chapter15/random_walk/rw_visual_final_pygal.py
@@ -6,11 +6,11 @@ rw = RandomWalk(5000)
 rw.fill_walk()
 
 # Making the XY chart with pigal.
-xy_chart = pygal.XY(stroke=False)
+xy_chart = pygal.XY(stroke=True)
 xy_chart.title = "Random Walk"
 
 # Utilizing zip to make a list of tuples [(0,1), (2,9)...]
-xy_chart.add("", list(zip(rw.x_values, rw.y_values)))
+xy_chart.add("Random Walk", list(zip(rw.x_values, rw.y_values)))
 
 # Rendering png and svg files of the exercise.
 xy_chart.render_to_file("rw_visual_final_pygal.svg")
