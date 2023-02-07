b86b2771ce0fe292e306da4d7f7e70b55961d772
DaveBit <dewidhoct@gmail.com>
2023-02-07 22:45:52 +0100

    Chapter 16 Downloading data

--- Chapter16_DownloadingData/highs_lows.py
+++ Chapter16_DownloadingData/highs_lows.py
@@ -11,25 +11,26 @@ with open(filename) as f:  # we open the file and store the file object in F
     header_row = next(reader)  # We use the next() function from the csv module.
     # And we retrieve the first line. Returns a list.
 
-    highs = []
+    dates, highs = [], []
     for row in reader:  # The reader stood in the second line, the first one was already read in line 6.
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)  # row[1] returns the max temperature parameter of each row.
 
-    print(highs)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))  # it is always a good practice to create a fig to add subplotes
 # or cover other needs.
-fig.canvas.set_window_title("Highs")
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # Format plot.
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
-plt.xlim(0)
 plt.show()
 """
 for index, column_header in enumerate(header_row):  # enumerate() gives you the index and the value.
